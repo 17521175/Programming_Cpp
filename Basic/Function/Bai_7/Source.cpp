4c1c29197dd9b510590634669448a87a5c4badbb
TrungNA <84486385+17521175@users.noreply.github.com>
2021-10-23 22:29:25 +0700

    Update Source.cpp

--- Basic/Function/Bai_7/Source.cpp
+++ Basic/Function/Bai_7/Source.cpp
@@ -14,7 +14,7 @@ double Sum(int n)
 int main()
 {
 	int n;
-    cout << "Tinh S(n) = 1 / 2 + 2 / 3 + 3 / 4 + ... + n / (n + 1)" << endl;
+    	cout << "Tinh S(n) = 1 / 2 + 2 / 3 + 3 / 4 + ... + n / (n + 1)" << endl;
 	cout << "Xin moi ban nhap n: ";
 	cin >> n;
 	cout << "Tong S(n) la: " << Sum(n) << endl;
