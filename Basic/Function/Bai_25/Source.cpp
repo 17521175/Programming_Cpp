11600e7ca4d81a6599dd32164473647fe7900ae8
TrungNA <84486385+17521175@users.noreply.github.com>
2021-10-23 23:21:12 +0700

    Update Source.cpp

--- Basic/Function/Bai_25/Source.cpp
+++ Basic/Function/Bai_25/Source.cpp
@@ -30,7 +30,7 @@ int Sum(int n)
 int main()
 {
 	cout <<"Tinh tong cac 'uoc so' chan cua so nguyen duong n" << endl;
-    int n = Check();
+    	int n = Check();
 	cout << "Tong cac uoc so chan cua so nguyen duong n la: " << Sum(n) << endl;
 	system("pause");
 	return 0;
