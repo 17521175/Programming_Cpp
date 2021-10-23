b0239309657598833b1986714eefa790b740456f
TrungNA <84486385+17521175@users.noreply.github.com>
2021-10-23 22:27:33 +0700

    Add files via upload

---
+++ Basic/Function/Bai_10/Source.cpp
@@ -0,0 +1,25 @@
+// Đề bài: Tính T(x, n) = x^n
+// Tên: Nguyễn Anh Trung
+// MSSV: 17521175
+#include <iostream>
+using namespace std;
+int LuyThua(int x, int n)
+{
+	int T = 1;
+	for (int i = 1; i <= n; i++) {
+		T = T*x;
+	}
+	return T;
+}
+int main()
+{
+	int x, n;
+    cout << "Tinh T(x, n) = x^n" << endl;
+	cout << "Xin moi ban nhap x: ";
+	cin >> x;
+	cout << "Xin moi ban nhap n: ";
+	cin >> n;
+	cout << "T(x, n) la: " << LuyThua(x,n) << endl;
+	system("pause");
+	return 0;
+}
---
+++ Basic/Function/Bai_5/Source.cpp
@@ -0,0 +1,23 @@
+// Đề bài: Tính S(n) = 1 + 1 / 3 + 1 / 5 + ... + 1 / (2n + 1)
+// Tên: Nguyễn Anh Trung
+// MSSV: 17521175
+#include <iostream>
+using namespace std;
+double Sum(int n)
+{
+	double S = 0;
+	for (int i = 1; i <= (2*n + 1); i+=2) {
+		S = S + 1.0 / i;
+	}
+	return S;
+}
+int main()
+{
+	int n;
+    cout << "Tinh S(n) = 1 + 1 / 3 + 1 / 5 + ... + 1 / (2n + 1)" << endl;
+	cout << "Xin moi ban nhap n: ";
+	cin >> n;
+	cout << "Tong S(n) la: " << Sum(n) << endl;
+	system("pause");
+	return 0;
+}
